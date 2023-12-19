c10ff6bcd01bc44cc724c34ed1d6970040998709
Ahmed_226 <88344628+ahm226@users.noreply.github.com>
2023-12-19 17:05:14 +0500

    localization

---
+++ lib/localization/languages/language_en.dart
@@ -0,0 +1,869 @@
+// ignore_for_file: non_constant_identifier_names
+import 'languages.dart';
+
+class LanguageEn extends Languages {
+  @override
+  String get GREENBRIDGELOGIN => "GREEN BRIDGE";
+
+  @override
+  String get Whoareyou => "Who are you";
+
+  @override
+  String get Voiceofusers => "Voice of Users";
+
+  @override
+  String get Voiceofusers2 => "Voice of Users";
+
+  @override
+  String get Add => "Add";
+
+  @override
+  String get General => "General";
+
+  @override
+  String get Content => "Content";
+
+  @override
+  String get Writethetitlemax80letters => "Write the title (max.80 letters)";
+
+  @override
+  String get Pleasewriteheremax1500letters =>
+      "Please write here (max.1500letters )";
+
+  @override
+  String get Addimages => "Add images";
+
+  @override
+  String get Save => "Save";
+
+  @override
+  String get Areyousureyouwanttodelete => "Are you sure you want to delete?";
+
+  @override
+  String get EditMyinfo => "Edit My info";
+
+  @override
+  String get Edit => "Edit";
+
+  @override
+  String get Pleaseinputpassword => "Please input password";
+
+  @override
+  String get Passwordconfirm => "Password confirm";
+
+  @override
+  String get Pleaseconfirmpassword => "Please confirm password";
+
+  @override
+  String get Changepassword => "Change password";
+
+  @override
+  String get Requestfortrialversion => "Request for trial version";
+  @override
+  String get Stories => "Stories";
+  @override
+  String get Save2 => "Save";
+  @override
+  String get Save3 => "Save";
+  @override
+  String get Findpassword => "Find password";
+
+  //
+  @override
+  String get Doyouwanttologout => "Do you want to Log out?";
+
+  @override
+  String get Cancel => "Cancel";
+
+  @override
+  String get OK => "Ok";
+
+  @override
+  String get oK => "Ok";
+
+  @override
+  String get writeareview => "Write a review";
+
+  @override
+  String get myreview => "My review";
+
+  @override
+  String get Inquiryoneoone => "1:1 Inquiry";
+
+  @override
+  String get InquirySentSuccessfully => "Inquiry Sent Successfully";
+
+  @override
+  String get Camera => "Camera";
+
+  @override
+  String get Album => "Album";
+
+  @override
+  String get whereyouwantimagesfrom => "where you want images from ?";
+
+  @override
+  String get Inquire => "Inquire";
+
+  @override
+  String get SorrynoDataisavailable => "Sorry no Date is available";
+
+//Login
+  @override
+  String get NEW_PRODUCT => "NEW PRODUCT";
+
+  @override
+  String get Total2 => "NEW PRODUCT";
+
+  @override
+  String get SALAD_PEA => "SALAD PEA";
+
+  @override
+  String get POST => "POST";
+
+  @override
+  String get Search => "Search";
+
+  @override
+  String get SpecialvegetablecultivationSystems =>
+      "Special vegetable cultivation Systems";
+
+  @override
+  String get Bitter => "Bitter";
+
+  @override
+  String get European => "European";
+
+  @override
+  String get recipies => "recipies";
+
+  @override
+  String get Introducingtherecipe => "Introducing the recipe";
+
+  @override
+  String get AvacadoEggBreakfastToast => "Avacado Egg Breakfast Toast";
+
+  @override
+  String get Sweet => "Sweet";
+
+  @override
+  String get American => "American";
+
+  @override
+  String get Special => "Special";
+
+  @override
+  String get BestProducts => "Best Products";
+
+  //SHOPPAGE
+  @override
+  String get KoppertCressSHOP => "Koppert Cress SHOP";
+
+  @override
+  String get RecommendedProducts => "Recommended Products";
+
+  //DisCovery
+  @override
+  String get Discovery => "Discovery";
+
+  @override
+  String get mins => "mins";
+
+  @override
+  String get FruitSalad => "Fruit Salad";
+
+  @override
+  String get cookingupsomebalsamicSteakgorgonzolasaladwithgrilledcorn =>
+      "cooking up some balsamic Steak gorgonzola salad with grilled corn";
+
+  //LIKe
+  @override
+  String get Product => "Product";
+
+  @override
+  String get Recipes => "Recipes";
+
+  @override
+  String get Posts => "Posts";
+
+  //SplashScreen
+  @override
+  String get GREENBRIDGE => "GREEN BRIDGE";
+
+  //LOGINSCREEN
+  @override
+  String get Welcometo => "Welcome to";
+
+  @override
+  String get ID => "ID";
+
+  @override
+  String get Email => "Email";
+
+  @override
+  String get Password => "Password";
+
+  @override
+  String get Login => "Login";
+
+  @override
+  String get Find => "Find";
+
+  @override
+  String get Joinin => "Join in";
+
+  @override
+  String get SignupformembershipeasilywithSNSaccount =>
+      "Sign up for membership easily with SNS account";
+
+//
+  @override
+  String get AddtoBag => "Add to Bag";
+
+  @override
+  String get AdjiCress => "Adji Cress";
+
+  @override
+  String get All => "All";
+
+  @override
+  String get AllProducts => "All Products";
+
+  @override
+  String get AvocadoToast => "Avocado Toast";
+
+  @override
+  String get BarcodeType => "Bar code Type";
+
+  @override
+  String get BasicMaterials => "Basic Materials";
+
+  @override
+  String get Best => "Best";
+
+  @override
+  String get BlueSorrel => "Blue Sorrel";
+
+  @override
+  String get Businessnumberforissuingtaxinvoices =>
+      "Business number for issuing tax invoices";
+
+  @override
+  String get Buy => "Buy";
+
+  @override
+  String get CHEFCOMMUNITY => "CHEF COMMUNITY";
+
+  @override
+  String get CHEFJUNG => "CHEF JUNG";
+
+  @override
+  String get CancellationorExchangeorReturnHistory =>
+      "Cancellation or Exchange or Return History";
+
+  @override
+  String get Capacity => "Capacity";
+
+  @override
+  String get Exchange => "Exchange";
+
+  @override
+  String get Validation =>
+      "English + Number + Special Character Combinations, 8 to 10 digits";
+
+  @override
+  String get Refund => "Refund";
+
+  @override
+  String get Deliverycheck => "Delivery check";
+
+  @override
+  String get CheckRootcondition => "Check Root condition";
+
+  @override
+  String get CheckingtheHydroponicnutrientsEC =>
+      "Checking the Hydroponic nutrients EC";
+
+  @override
+  String get Checkleafstatus => "Check leaf status";
+
+  @override
+  String get Checkout => "Checkout";
+
+  @override
+  String get Checkpassword => "Check password";
+
+  @override
+  String get Checkthecleanlinessofthebottombed =>
+      "Check the cleanliness of the bottom bed";
+
+  @override
+  String get Chef => "Chef";
+
+  @override
+  String get Comment => "Comment";
+
+  @override
+  String get CompanyInfo => "Company Info";
+
+  @override
+  String get Countryoforigin => "Country of origin";
+
+  @override
+  String get CultivationManagement => "Cultivation Management";
+
+  @override
+  String get CustomerService => "Customer Service";
+
+  @override
+  String get CustomersFeedback => "Customers Feedback";
+
+  @override
+  String get Cutthecabbageintofinelongshredsandthendippedincoldwater =>
+      "Cut the cabbage into fine long shred sand then dipped in cold water";
+
+  @override
+  String get Data => "Data";
+
+  @override
+  String get Deletemyaccount => "Delete my account";
+
+  @override
+  String get Difficulty => "Difficulty";
+
+  @override
+  String get Doyouwanttouseapilotcress => "Do you want to use a pilot cress";
+
+  @override
+  String get Easytocookanddelicious => "Easy to cook and delicious";
+
+  @override
+  String get EssentialTermsandConditions => "(Essential) Terms and Conditions";
+
+  @override
+  String get FAQ => "FAQ";
+
+  @override
+  String get Farmer => "Farmer";
+
+  @override
+  String get Cheff => "Cheff";
+
+  @override
+  String get Generalpopup => "General";
+
+  @override
+  String get Phoneno => " Phone No";
+
+  @override
+  String get Thisisyouremail => "This is your Email ID";
+
+  @override
+  String get Finishedharvestingofokrastem => "Finished harvesting of okra stem";
+
+  @override
+  String get GreenSorrel => "Green Sorrel";
+
+  @override
+  String get GreenSouffle => "Green Souffle";
+
+  @override
+  String get Harvesting => "Harvesting";
+
+  @override
+  String get Home => "Home";
+
+  @override
+  String get Humd => "Humd";
+
+  @override
+  String get Ingredients => "Ingredients";
+
+  @override
+  String get InquiryHistory => "Inquiry History";
+
+  @override
+  String get Italianpasta => "Italian pasta";
+
+  @override
+  String get Itisbeingcultivatedonatrailbasis =>
+      "It is being cultivated on a trail basis";
+
+  @override
+  String get Iwilluseit => "I will use it";
+
+  @override
+  String get Koppert => "Koppert";
+
+  @override
+  String get LetmeKnowwhatcropsyouneed => "Let me Know what crops you need";
+
+  @override
+  String get LetmeknowtheQuantitytopurchase =>
+      "Let me know the Quantity to purchase";
+
+  @override
+  String get Light => "Light";
+
+  @override
+  String get Like => "Like";
+
+  @override
+  String get Liquidinwater => "Liquid in water";
+
+  @override
+  String get Logout => "Log out";
+
+  @override
+  String get Medium => "Medium";
+
+  @override
+  String get MintCress => "Mint Cress";
+
+  @override
+  String get MixallingredientsforthevinaigretteMixthesaladwiththeSakuraCressaddsomevinaigretteandtaste =>
+      "Mix all ingredients for the vinaigrette Mix the salad with the Sakura Cress add some vinaigrette and taste";
+
+  @override
+  String get ModifyMemberinfo => "Modify Member info";
+
+  @override
+  String get MonitoringCultivation => "Monitoring Cultivation";
+
+  @override
+  String get MyInformationManagement => "My Information Management";
+
+  @override
+  String get MyOrders => "My Orders";
+
+  @override
+  String get MyPageChef => "My Page Chef";
+
+  @override
+  String get MyShoppingInfo => "My Shopping Info";
+
+  @override
+  String get Name => "Name";
+
+  @override
+  String get New => "New";
+
+  @override
+  String get Numberofitemsordered => "Number of items ordered";
+
+  @override
+  String get Okra => "Okra";
+
+  @override
+  String get myOrder => "My Order";
+
+  @override
+  String get OrderTotal => "Order Total";
+
+  @override
+  String get OrganicbassilraisedbyPfarm => "Organic bassil raised by P farm";
+
+  @override
+  String get Origin => "Origin";
+
+  @override
+  String get PISTACHIOCHOCOLATEMENDIANTS => "PISTACHIO CHOCOLATE MENDIANTS";
+
+  @override
+  String get Phonenumber => "Phone number";
+
+  @override
+  String get PlacebutterinaheatedpanandeggsbackandforthtomatchthesizeofthebreadGrilluntilgolden =>
+      "Place butter in a heated pan and eggs back and forth to match the size of the bread Grill until golden";
+
+  @override
+  String get Plantingbasicseeds => "Planting basic seeds";
+
+  @override
+  String get PleaseScanQRCode => "Please Scan QR Code";
+
+  @override
+  String get Pleasecheckpassword => "Please check password";
+
+  @override
+  String get Pleaseleaveyourinformation => "Please leave your information";
+
+  @override
+  String get PleaseletmeKnowthesubscriptionconditions =>
+      "Please let me Know the subscription conditions";
+
+  @override
+  String get ProductNumber => "Product Number";
+
+  @override
+  String get ProductRequiredInformation => "Product Required Information";
+
+  @override
+  String get Productcomposition => "Product composition";
+
+  @override
+  String get ProductsusedinthisRecipe => "Products used in this Recipe";
+
+  @override
+  String get PurchasedProducts => "Purchased Products";
+
+  @override
+  String get QRCode => "QR Code";
+
+  @override
+  String get QuantityofPurcahse => "Quantity of Purcahse";
+
+  @override
+  String get Refertotheproductdetails => "Refer to the product details";
+
+  @override
+  String get Register => "Register";
+
+  @override
+  String get RelatedProducts => "Related Products";
+
+  @override
+  String get RelatedRecipes => "Related Recipes";
+
+  @override
+  String get RelatedtoCS => "Related to CS";
+
+  @override
+  String get Requestfortrailversion => "Request for trail version";
+
+  @override
+  String get Requesttouse => "Request to use";
+
+  @override
+  String get ResetCondition => "Reset Condition";
+
+  @override
+  String get Result => "Result";
+
+  @override
+  String get Reviews => "Reviews";
+
+  @override
+  String get RockChivesischaracterisedbythelittleblackdotatthetop =>
+      "Rock Chives is characterised by the little black dot at the top";
+
+  @override
+  String get Salty => "Salty";
+
+  @override
+  String
+      get SeasontheduckfilletsandfrymediumrarePeelthegrapefruitavifremovetheslicesandcutintosmallerpieces =>
+          "ID";
+
+  @override
+  String get SeeDetails => "See Details";
+
+  @override
+  String get SelectDeliverytime => "Select Delivery time";
+
+  @override
+  String get SelectPackingUnit => "Select Packing Unit";
+
+  @override
+  String get SelectPurchaseQuantity => "Select Purchase Quantity";
+
+  @override
+  String get SelectQuatitybysubscriptionday =>
+      "Select Quatity by subscription day";
+
+  @override
+  String get Selectsubscriptionperiod => "Select subscription period";
+
+  @override
+  String get Selectsubscriptionstartdate => "Select subscription start date";
+
+  @override
+  String get Shipment => "Shipment";
+
+  @override
+  String get Shop => "Shop";
+
+  @override
+  String get ShoppingBag => "Shopping Bag";
+
+  @override
+  String get Sorrel => "Sorrel";
+
+  @override
+  String get Sour => "Sour";
+
+  @override
+  String get Step => "Step";
+
+  @override
+  String get Step1 => "Step 1";
+
+  @override
+  String get Step2 => "Step 2";
+
+  @override
+  String get Subscribe => "Subscribe";
+
+  @override
+  String get SubscriptionApplication => "Subscription Application";
+
+  @override
+  String get SubscriptionProducts => "Subscription Products";
+
+  @override
+  String get SubscriptionTotal => "Subscription Total";
+
+  @override
+  String get SweetBassil => "Sweet Bassil";
+
+  @override
+  String get SweetScentOFVanilla => "Sweet Scent OF Vanilla";
+
+  @override
+  String get SyrhaLeaves => "Syrha Leaves";
+
+  @override
+  String get SyrhaLeavesaretheseedling => "Syrha Leaves are the seedling";
+
+  @override
+  String get TODO => "TO DO";
+
+  @override
+  String get TakeaLookatthecropsundertrialcultivation =>
+      "Take a Look at the crops under trial cultivation";
+
+  @override
+  String get Taste => "Taste";
+
+  @override
+  String get TasteFriends => "Taste Friends";
+
+  @override
+  String get Temp => "Temp";
+
+  @override
+  String get TheflavourofSyrhaLeaves => "The flavour of Syrha Leaves";
+
+  @override
+  String
+      get TheflavourofSyrhaLeavesreallycomesthroughinfruitsaladsorincombinationswithredfruitwhitechocolateorevencoriander =>
+          "ID";
+
+  @override
+  String get TodaykeyTasks => "Today key Tasks";
+
+  @override
+  String get TodaysKeyTasks => "Todays Key Tasks";
+
+  @override
+  String get Total => "Total";
+
+  @override
+  String get Transplant => "Transplant";
+
+  @override
+  String get TransplantorShipment => "Transplant or Shipment";
+
+  @override
+  String get TrialcultivationAAA => "Trial cultivation AAA";
+
+  @override
+  String get Triallemonbalm => "Trial lemon balm";
+
+  @override
+  String get Typehere => "Type here";
+
+  @override
+  String get Umami => "Umami";
+
+  @override
+  String get Weeklyirrigationmonitoring => "Weekly irrigation monitoring";
+
+  @override
+  String get WewillgrowtheCressyouneed => "We will grow the Cress you need";
+
+  @override
+  String get WhatisSyrhaLeaves => "What is Syrha Leaves";
+
+  @override
+  String get WhychooseSyrhaLeaves => "Why choose Syrha Leaves";
+
+  @override
+  String get cookingupsomebalsamicsteakgorgonzolasaladwithgrilledcorn =>
+      "cooking up some balsamic steak gorgonzola salad with grilled corn";
+
+  @override
+  String get daysleft => "days left";
+
+  @override
+  String get heads => "heads";
+
+  @override
+  String get koppertcress => "koppert cress";
+
+  @override
+  String get min => "min";
+
+  @override
+  String get noPermission => "no Permission";
+
+  @override
+  String get ofcontentbypackaging => "of content by packaging";
+
+  @override
+  String get other => "other";
+
+  @override
+  String get DeliveryTotal => "DeliveryTotal";
+
+  @override
+  String get shead => "shead";
+
+  @override
+  String get spearmint => "spear mint";
+
+  @override
+  String get sweetbassil => "sweet bassil";
+
+  ///
+
+  @override
+  String get RockChives => "Rock Chives";
+
+  @override
+  // TODO: implement CO2
+  String get CO2 => "CO2";
+
+  @override
+  // TODO: implement EC
+  String get EC => "EC";
+
+  @override
+  // TODO: implement PH
+  String get PH => "Ph";
+
+  @override
+  String get MyPageFarmer => "My Page Farmer";
+
+  @override
+  String get YesIwillparticipate => "Yes I will participate";
+
+  @override
+  String get Pleasegrowthiscrop => "Please grow this crop";
+
+  @override
+  String get Whatareyouraising => "What are you raising";
+
+  @override
+  String get Monitoring => "Monitoring";
+
+  @override
+  String get Cultivation => "Cultivation";
+
+  @override
+  String get korean => "Korean";
+
+  @override
+  String get SearchmyID => "Search my ID";
+
+  @override
+  String get Searchmypw => "Search my pw";
+
+  @override
+  String get FARMERSCOMMUNITY => "COMMUNITY";
+
+  @override
+  String get Joinasagrower => "Join as a grower";
+
+  @override
+  String get RequestSentSuccessfully => "Request sent Successfully";
+
+  @override
+  String get Applyforparticipation => "Apply for participation";
+
+  @override
+  String get Pleasetellmewhatvegetablesyouwanttosell =>
+      "Please tell me what vegetables you want to sell";
+
+  @override
+  String get Canyouparticipateasagrowerofthiscress =>
+      "Can you participate as a grower of this cress";
+
+  @override
+  String get Inspiration => "Inspiration";
+
+  @override
+  String get Boragecress => "Borage cress";
+
+  @override
+  String get Culinarygreens => "Culinary greens";
+
+  @override
+  String get Herbs => "Herbs";
+
+  @override
+  String get EdibleFlowers => "Edible Flowers";
+
+  @override
+  String get SaladGreens => "Salad Greens";
+
+  @override
+  String get RootGreens => "Root Green";
+
+  @override
+  String get PersonalPolicy => "Personal Policy";
+
+  @override
+  String get TermAndCondition => "Term And Condition";
+
+  @override
+  String get Entercultivationinformation => "Enter cultivation information";
+
+  @override
+  String get BenchID => "Bench ID";
+
+  @override
+  String get ChooseCress => "Choose Cress";
+
+  @override
+  String get InputQuantity => "Input Quantity";
+
+  @override
+  String get Startdateofcultivation => "Start date of cultivation";
+
+  @override
+  String get Specials => "Specials";
+
+  @override
+  String get Starters => "Starters";
+
+  @override
+  String get Mains => "Mains";
+
+  @override
+  String get Desserts => "Desserts";
+
+  @override
+  String get AmthonyBourdain => "Amthony Bourdain";
+
+  @override
+  String get Startdate => "Start date";
+
+  @override
+  String get Enddate => "End date";
+
+  @override
+  String get Barcode => "Bar code";
+
+  @override
+  String get Itemname => "Itemname";
+
+  @override
+  String get Quantity => "Quantity";
+
+  @override
+  String get Cultivationnumber => "Cultivation number";
+
+  @override
+  String get KoppertCress => "Koppert Cress";
+}
